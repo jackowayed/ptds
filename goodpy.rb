0ddbfbed563be0a20dc5126a7ef3119b1e845cb5
Daniel Jackoway <danjdel@gmail.com>
2009-04-14 18:36:18 -0400

    checks for most keywords. much less doing necessary

--- goodpy.rb
+++ goodpy.rb
@@ -10,11 +10,20 @@ def good_to_normal(filename)
   end
 end
 def indent_after?(line)
-  /do$/ =~ line
+  starts_with_indenting_keyword? line || /do$/ =~ line
+end
+def starts_with_indenting_keyword?(line)
+  %w(def if else for class try while).each do |keyword|
+    return true if Regexp.new("^"+keyword) =~ line
+  end
+  false
 end
 def unindent_before?(line)
   "end" == line
 end
+#need to comment out the dos and ends
+#comment mostly so that line #s stay the same
+#(deleting ends would delete a line)
 def comment_ruby_syntax(line)
   if unindent_before? line
     "#" + line
